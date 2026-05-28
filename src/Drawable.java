e25b23987d9fd00e5c7add2f81e43aa6af6fbd2b
Shah-Aditya <104385858+minecrafttest1@users.noreply.github.com>
2026-05-27 20:33:37 -0700

    Add comment to draw method in Drawable interface

--- src/Drawable.java
+++ src/Drawable.java
@@ -1,5 +1,6 @@
 import java.awt.Graphics2D;
 
+// every object that gets drawn on screen has to implement this interface
 public interface Drawable {
-    void draw(Graphics2D g);
+    void draw(Graphics2D g); // each class does the drawing itself
 }
